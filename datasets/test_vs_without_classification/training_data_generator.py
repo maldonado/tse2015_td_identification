95a4ecdb30d86609f047a957d0e2107c20debf10
Everton Maldonado <everton.maldonado@gmail.com>
2015-06-22 14:55:58 -0400

    compare test with without classification

---
+++ datasets/test_vs_without_classification/training_data_generator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import os
+import sys
+import psycopg2
+
+# database = sys.argv[2]
+password = sys.argv[1]
+
+def write_in_file(result):
+    with open ('classified_seq.train','a') as classified_seq:
+        for line in result:
+            classified_seq.write("{0}\t{1}\n".format(line[0], line[1].replace('\n','').replace('\r\n', '').replace('\r', '')))
+
+try:
+    connection = None
+    
+    # connect to the database to retrieve the file name linked with the commit
+    connection = psycopg2.connect(host='localhost', port='5432', database='comment_classification', user='evermal', password= password)
+    cursor = connection.cursor()
+ 
+    cursor.execute("select a.classification, a.commenttext from processed_comment a, comment_class b where a.commentclassid = b.id  and b.projectname like '%apache-jmeter-2.10%' and classification in ('WITHOUT_CLASSIFICATION', 'TEST')")
+    write_in_file(cursor.fetchall())
+
+    cursor.execute("select a.classification, a.commenttext from processed_comment a, comment_class b where a.commentclassid = b.id  and b.projectname like '%apache-ant%' and classification in ('WITHOUT_CLASSIFICATION', 'TEST')")
+    write_in_file(cursor.fetchall())
+
+    cursor.execute("select a.classification, a.commenttext from processed_comment a, comment_class b where a.commentclassid = b.id  and b.projectname like '%jfreechart%' and classification in ('WITHOUT_CLASSIFICATION', 'TEST')")
+    write_in_file(cursor.fetchall())
+
+    cursor.execute("select a.classification, a.commenttext from processed_comment a, comment_class b where a.commentclassid = b.id  and b.projectname like '%columba%' and classification in ('WITHOUT_CLASSIFICATION', 'TEST')")
+    write_in_file(cursor.fetchall())
+
+except Exception, e:
+    raise e
