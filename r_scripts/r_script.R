1ca85e6e6b1ffb890e54ae6a72893f2a364aa142
Everton Maldonado <everton.maldonado@gmail.com>
2015-09-04 18:24:44 -0400

    fix bug in the analysis and finished reviewing rq1

--- r_scripts/r_script.R
+++ r_scripts/r_script.R
@@ -9,7 +9,7 @@ barplot(as.matrix(mydata),  ylab="Percentage",  ylim=c(0, 1), beside=TRUE, col= 
 legend("topright", c("F1 measure", "Baseline F1 measure", "Rdn F1 measure"), cex=2, fill=terrain.colors(3))
 
 # generate figure to compare f1measure, baseline f1measure and rdn F1 measure for all projects (requirement debt)
-mydata <- data.frame(Ant = c(0.987,0.142,0.008), Jmeter = c(0.255,0.108,0.005), ArgoUML = c(0.760,0.040,0.125), Columba = c(0.934,0.065, 0.04),  SQuirrel = c(0.875,0.044, 0.04), Hibernate = c(0.476,0.118,0.042), EMF = c(0.381,0.087,0.007), JFreeChart = c(0.500,0.044,0.011), JRuby = c(0.462,0.078,0.045), JEdit = c(0.091,0.264,0.003))
+mydata <- data.frame(Ant = c(0.364,0.142,0.008), Jmeter = c(0.255,0.108,0.005), ArgoUML = c(0.760,0.040,0.125), Columba = c(0.934,0.065, 0.04),  SQuirrel = c(0.875,0.044, 0.04), Hibernate = c(0.476,0.118,0.042), EMF = c(0.381,0.087,0.007), JFreeChart = c(0.500,0.044,0.011), JRuby = c(0.462,0.078,0.045), JEdit = c(0.091,0.264,0.003))
 barplot(as.matrix(mydata),  ylab="Percentage",  ylim=c(0, 1), beside=TRUE, col= terrain.colors(3), cex.axis=1.5, cex.names=1.2 )
 legend("topright", c("F1 measure", "Baseline F1 measure", "Rdn F1 measure"), cex=2, fill=terrain.colors(3))
 
