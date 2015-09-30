b31e1613ad11f027f045a87d9c89843fe168576e
Everton Maldonado <everton.maldonado@gmail.com>
2015-09-30 04:34:52 -0400

    create new ranking of most common words

--- r_scripts/r_script.R
+++ r_scripts/r_script.R
@@ -4,14 +4,15 @@ barplot(as.matrix(mydata),  ylab="Percentage",  ylim=c(0, 1), beside=TRUE, col= 
 legend("topright", c("Precision", "Recall", "F1","Baseline P", "Baseline R", "Baseline F1"), cex=0.8, fill=terrain.colors(6))
 
 # generate figure to compare f1measure, baseline f1measure and rdn F1 measure for all projects (design debt)
-mydata <- data.frame(Ant = c(0.517,0.142,0.045) ,Jmeter = c(0.731,0.108,0.075),ArgoUML = c(0.814,0.040,0.151),Columba = c(0.601,0.065,0.038),SQuirrel = c(0.540,0.044,0.055),Hibernate = c(0.744,0.118,0.214),EMF = c(0.470,0.087,0.035),JFreeChart = c(0.492,0.044,0.080),JRuby = c(0.783,0.078,0.131),JEdit = c(0.509,0.264,0.037))
-barplot(as.matrix(mydata),  ylab="Percentage",  ylim=c(0, 1), beside=TRUE, col= terrain.colors(3), cex.axis=1.5, cex.names=1.2 )
-legend("topright", c("F1 measure", "Baseline F1 measure", "Naive Baseline F1 measure"), bty = "n" , cex=2, fill=terrain.colors(3))
+mydata <- data.frame(Ant = c(0.517,0.142,0.045) ,Jmeter = c(0.731,0.108,0.075),ArgoUML = c(0.814,0.040,0.151),Columba = c(0.601,0.065,0.038),SQuirrel = c(0.540,0.044,0.055),Hibernate = c(0.744,0.118,0.214),EMF = c(0.470,0.087,0.035),JFreeChart = c(0.492,0.044,0.080), JEdit = c(0.509,0.264,0.037), JRuby = c(0.783,0.078,0.131) )
+barplot(as.matrix(mydata),  ylim=c(0, 1), beside=TRUE, col= terrain.colors(3), cex.lab=2,  cex.axis=2.3, cex.names=2.3, mgp = c(3, 2, 1) )
+legend(24, 1, c("F1 measure", "Baseline F1 measure", "Naive Baseline F1 measure"), bty = "n" , cex=2.3, fill=terrain.colors(3))
+
 
 # generate figure to compare f1measure, baseline f1measure and rdn F1 measure for all projects (requirement debt)
-mydata <- data.frame(Ant = c(0.364,0.142,0.008), Jmeter = c(0.255,0.108,0.005), ArgoUML = c(0.760,0.040,0.125), Columba = c(0.934,0.065, 0.04),  SQuirrel = c(0.875,0.044, 0.04), Hibernate = c(0.476,0.118,0.042), EMF = c(0.381,0.087,0.007), JFreeChart = c(0.500,0.044,0.011), JRuby = c(0.462,0.078,0.045), JEdit = c(0.091,0.264,0.003))
-barplot(as.matrix(mydata),  ylab="Percentage",  ylim=c(0, 1), beside=TRUE, col= terrain.colors(3), cex.axis=1.5, cex.names=1.2 )
-legend("topright", c("F1 measure", "Baseline F1 measure", "Naive Baseline F1 measure"), bty = "n" , cex=2, fill=terrain.colors(3))
+mydata <- data.frame(Ant = c(0.364,0.142,0.008), Jmeter = c(0.255,0.108,0.005), ArgoUML = c(0.760,0.040,0.125), Columba = c(0.934,0.065, 0.04),  SQuirrel = c(0.875,0.044, 0.04), Hibernate = c(0.476,0.118,0.042), EMF = c(0.381,0.087,0.007), JFreeChart = c(0.500,0.044,0.011), JEdit = c(0.091,0.264,0.003), JRuby = c(0.462,0.078,0.045))
+barplot(as.matrix(mydata),  ylim=c(0, 1), beside=TRUE, col= terrain.colors(3), cex.lab=2,  cex.axis=2.3, cex.names=2.3, mgp = c(3, 2, 1) )
+legend("topright", c("F1 measure", "Baseline F1 measure", "Naive Baseline F1 measure"), bty = "n" , cex=2.3, fill=terrain.colors(3))
 
 
 
@@ -63,26 +64,20 @@ postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classif
 library(RPostgreSQL)
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, host='localhost', port='5432', dbname='comment_classification', user='evermal', password='evermalton')
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%ant%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%jmeter%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%argo%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%columba%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%emf%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%hibernate%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%jEdit%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%jfreechart%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%jruby%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
-postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%sql12%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
+# postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%ant%' and category = 'DESIGN' and classificationid=3  order by 1,2")
+
+postgresql <- dbSendQuery(con, "select projectname, projectstrainedwith, classifiedf1 , classifiedrandomf1, baselinef1 from classifier_results where projectname like '%argo%' and category = 'IMPLEMENTATION' and classificationid=3  order by 1,2")
+
 data1 <- fetch(postgresql, n=-1)
 dim(data1)
 dbHasCompleted(postgresql)
-plot(data1$projectstrainedwith, data1$classifiedf1*100, type="b",
-     col="red", lty=2, pch=2, lwd=2,
-     xlab="Projects used in training data", ylab="F1 measure",
-     xlim=c(0, 10), ylim=c(0, 100))
-lines(data1$projectstrainedwith, data1$classifiedrandomf1*100, type="b", pch=22, col="blue", lty=2)
-lines(data1$projectstrainedwith, data1$baselinef1*100, type="b", pch=21, col="darkgreen", lty=2)
-legend(8, 80, c("Classifier", "Baseline", "Random" ), col= c("red", "darkgreen",  "blue"), pch = c(2, 21, 22), bty = "n" , cex=0.9)
+plot(data1$projectstrainedwith, data1$classifiedf1, type="b",
+     col="red", lty=0, pch=2, lwd=2,
+     xlab="Projects used in training dataset", ylab="F1 measure",
+     xlim=c(0, 10), ylim=c(0, 1))
+lines(data1$projectstrainedwith, data1$classifiedrandomf1, type="b", pch=22, col="blue", lty=0)
+lines(data1$projectstrainedwith, data1$baselinef1, type="b", pch=21, col="darkgreen", lty=0)
+legend(8, 0.99, c("Our approach", "Baseline", "Naive baseline" ), col= c("red", "darkgreen",  "blue"), pch = c(2, 21, 22), bty = "n" , cex=0.9)
 
 
 # Cohen's kappa FOR THE DATASET
